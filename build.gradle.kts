db2eb940aaa2a174de861c2e33ac5e81ee7c7b29
Toray-Life <stypmabichek@gmail.com>
2023-03-27 21:38:46 +0400

    Add Moshi

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,13 +8,15 @@ repositories {
 }
 
 dependencies {
-    runtimeOnly("org.jetbrains.kotlin:kotlin-scripting-jsr223:1.7.20")
+    implementation("org.jetbrains.kotlin:kotlin-scripting-jsr223:1.7.20")
+    implementation("com.squareup.moshi:moshi-kotlin:1.14.0")
+    implementation(kotlin("reflect"))
     runtimeOnly("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.6.4")
 }
-
+val myversion = 0.1
 tasks {
     shadowJar {
-        archiveBaseName.set("Kotlin-Scripting")
+        archiveBaseName.set("Kotlin-Scripting-$myversion")
         archiveVersion.set("")
         archiveClassifier.set("")
 
